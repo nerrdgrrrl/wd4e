95750557fb4bb47e0028320b956a360f4d3f8617
Amy N. Thomas <amythomas@stanford.edu>
2025-07-03 14:43:03 -0400

    Add files via upload

---
+++ capstone/js/click.js
@@ -0,0 +1,36 @@
+/* change background-color on click */
+
+function openWeb(){
+      var webDesign = document.getElementById("web");
+        webDesign.classList.toggle("hilite");
+       }
+function openGraphic(){
+      var graphicDesign = document.getElementById("graphic");
+        graphicDesign.classList.toggle("hilite");
+       }
+function openIllustration(){
+      var illusDesign = document.getElementById("illustration");
+        illusDesign.classList.toggle("hilite");
+       }              
+ 
+// function unDo(){
+//     //   var unSet = document.getElementById("web");
+//     //     unSet.classList.remove("hilite");
+//       var unSet = document.getElementById("web");
+//         unSet.classList.remove("hilite");              
+
+// }
+
+
+// function upDate(){
+//     Array.from(document.getElementsByClassName('openDiv'));
+//     const webDesign = document.getElementById('web');
+//     const links = document.querySelectorAll('.nav-link');
+
+//     links.addEventListener('click', function() {
+//         links.forEach(link => {
+//             link.classList.toggle('hilite');
+//         });
+//     });
+// }
+
